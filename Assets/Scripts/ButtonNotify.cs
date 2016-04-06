a6fd5e86a3cfbf7fdab3df704dccdeba814e0bea
zhangjie <zhangjie0072@126.com>
2016-04-06 11:18:02 +0800

    first commit

---
+++ Assets/Scripts/ButtonNotify.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonNotify : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void LabelBtnClick(){
+		print("label btn clicked");
+	}
+
+	public void SpriteBtnClick(){
+		print("sprite btn clicked");
+	}
+}
